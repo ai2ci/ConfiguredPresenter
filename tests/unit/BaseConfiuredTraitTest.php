983043a8b1612584695a732f871003e303d30c0c
ai2ci <ai2ci@ai-lubuntu>
2016-10-27 15:48:23 +0200

    test

--- tests/unit/BaseConfiuredTraitTest.php
+++ tests/unit/BaseConfiuredTraitTest.php
@@ -1,7 +1,9 @@
 <?php
+use ConfiguredPresenters\BaseConfiguredTrait;
 
 class BaseConfiuredTraitTest extends \PHPUnit_Framework_TestCase
 {
+    use BaseConfiguredTrait;
 
     protected function setUp()
     {
@@ -11,5 +13,29 @@ class BaseConfiuredTraitTest extends \PHPUnit_Framework_TestCase
     protected function tearDown()
     {
         
+    }
+    
+    public function testInitSimpleVariable()
+    {
+        $this->initVariables([
+            'test' => 'value'
+        ]);
+        $this->isType('string')->evaluate($this->test);
+        $this->assertEquals('value', $this->test);
+    }
+    public function testInitArray()
+    {
+        $this->initVariables([
+            'test' => 'value',
+            'testArray' => [],
+            'testInteger' => 123,
+            'testNull' => null,
+            'template' => null,
+        ]);
+        $this->isType('string')->evaluate($this->test);
+        $this->isType('array')->evaluate($this->testArray);
+        $this->isType('int')->evaluate($this->testInteger);
+        $this->isNull()->evaluate($this->testNull);
+        $this->assertObjectNotHasAttribute('template', $this);
     }
 }

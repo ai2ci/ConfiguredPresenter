a3bd1dfb5f75d37725c1cfa0a0ccaf5606b1601f
ai2ci <ai2ci@ai-lubuntu>
2016-10-27 13:19:34 +0200

    codeception

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,4 +1,9 @@
 <?php
 
+
+require_once __DIR__ . '/../src/BaseConfiguredTrait.php';
+require_once __DIR__ . '/../src/ConfigLoader.php';
+require_once __DIR__ . '/../src/ConfigParser.php';
+require_once __DIR__ . '/../src/VisitCounter.php';
 require_once __DIR__ . '/../vendor/autoload.php';
 // This is global bootstrap for autoloading
